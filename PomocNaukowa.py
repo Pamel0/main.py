f689d1e505ad685d5021445f763d47cd3211aaa4
Pamel0 <klaudiuszgrzeszczyk@op.pl>
2022-05-20 22:08:00 +0200

    Initial commit

---
+++ PomocNaukowa.py
@@ -0,0 +1,38 @@
+from math import *
+#zad1
+# e = exp(5)
+# cosinus = cos(15)**2
+# pierwiastek = (5/36) ** 1/5
+# wynik = pierwiastek + e + cosinus
+# print(round(wynik, 2))
+
+#zad2
+# lista = [1,2,3,4,5,6,7,8,3.14,5.21]
+# a = 0
+# b = 0
+# for z in lista:
+#     if type(z)==int:
+#         a +=1
+#     else:
+#         b +=1
+# print(a)
+# print(b)
+
+#zad3
+# lista=[1,2,3,3.14,'a','b']
+# def funkcja(listaliczbowa):
+#     listaliczbowa=[]
+#     for g in lista:
+#         if type(g) != str:
+#             listaliczbowa.append(g)
+#     print("To sa wszystkie liczby:", (listaliczbowa))
+#     print("suma tych liczb to:", sum(listaliczbowa))
+# funkcja(lista)
+
+
+#zad4
+# try:
+#     oc=int(input("Podaj liczbe w systemie dziesietnym: "))
+#     print("Convert...", oct(oc))
+# except ValueError:
+#     print("To nie jest liczba w systemie dziesietnym")
