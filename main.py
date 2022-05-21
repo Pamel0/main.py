014507caff8e7687348223df7a2ead7d9d4a4bc1
Pamel0 <93038245+Pamel0@users.noreply.github.com>
2022-05-21 22:51:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
-from math import *
+import math
 #zad1
-# e = exp(5)
-# cosinus = cos(15)**2
-# pierwiastek = (5/36) ** 1/5
-# wynik = pierwiastek + e + cosinus
-# print(round(wynik, 2))
-
+# nawias = ((4/5) ** 2)
+# sinus = math.sin(65)
+# wynik = ((sinus + nawias) ** (1/4))
+# print(round(wynik, 3))
 #zad2
-# lista = [1,2,3,4,5,6,7,8,3.14,5.21]
-# a = 0
-# b = 0
-# for z in lista:
-#     if type(z)==int:
-#         a +=1
-#     else:
-#         b +=1
-# print(a)
-# print(b)
-
+# listo=['a', 'b', 'c', 'd', 1, 2, 3]
+# def funkcja(lista1):
+#
+#     lista1=[]
+#     lista2=[]
+#     for i in listo:
+#         if type(i)==int:
+#             lista1.append(i)
+#         else:
+#             lista2.append(i)
+#
+#     print(lista1)
+#     print(lista2)
+#     print("Liczb jest:", len(lista1))
+#     print("Znakow jest:", len(lista2))
+# funkcja(listo)
+# print('\n')
 #zad3
-# lista=[1,2,3,3.14,'a','b']
-# def funkcja(listaliczbowa):
-#     listaliczbowa=[]
-#     for g in lista:
-#         if type(g) != str:
-#             listaliczbowa.append(g)
-#     print("To sa wszystkie liczby:", (listaliczbowa))
-#     print("suma tych liczb to:", sum(listaliczbowa))
-# funkcja(lista)
-
-
+# słownik = {1: 22, 2: 22, 3:33, "cos":"ktos", 6:53}
+# lista = []
+# print(len(słownik))
+#
+# for i in słownik:
+#     if(type(i)==int):
+#         lista.append(słownik[i])
+# print("Klucze liczbowe:", len(lista))
 #zad4
 # try:
-#     oc=int(input("Podaj liczbe w systemie dziesietnym: "))
-#     print("Convert...", oct(oc))
+#     a = int(input("Podaj a:"))
+#     lista = [1, 2, 3, 4]
+#     lista1 = []
+#     for i in lista:
+#         if a > i:
+#             lista1.append(i)
 # except ValueError:
-#     print("To nie jest liczba w systemie dziesietnym")
+#     print("To nie jest liczba!")
+#
+#     print(lista1)
+# print("liczb mniejszych jest:", len(lista1))
+
+
+
+
+
+
